6ef12f0277151d8d8652eb35e6c0f7c800f923ed
OgliariNatan <natanogliari@gmail.com>
2023-08-29 10:21:50 -0300

    add outro exemplo sem statica

---
+++ Matematica/src/main/java/com/mycompany/matematica/Matematica.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.matematica;
+
+/**
+ *
+ * @author AULA-1
+ * 
+ * Este exemplo não é o mais indicado pois a classe não é statica.
+ * 
+ * 
+ * 
+ */
+public class Matematica {
+    
+    double PI = 3.141516;
+    double E = 2.7182818284;
+    double PHI = 1.6180339887;
+    
+    int soma(int a, int b){// Realiza a adição de dois numeros
+        return a + b ;
+    }
+    
+    int mult(int a, int b){ //realiza a multiplicação de dois numeros
+        return a * b;
+    }
+    
+    public static void main(String[] args) {
+        
+        Matematica mat = new Matematica(); //instancia a classe
+        
+        System.out.println("Numero Pi: " + mat.PI);
+        System.out.println("Numero E: " + mat.E);
+        System.out.println("Numero Phi: " + mat.PHI);
+        System.out.println("Soma: " + mat.soma(5, 3));
+        System.out.println("Multi: " + mat.mult(5, 3));  
+        
+        
+        System.out.println("FIM da execução");
+    }
+}
