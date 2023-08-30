62ecb92c6d540370450ecf5f758163bab42a0ea0
OgliariNatan <natanogliari@gmail.com>
2023-08-30 09:30:46 -0300

    adicioonado projeto aula pratica

---
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciabanco;
+
+/**
+ *
+ * @author AULA-1
+ */
+public class GerenciaBanco {
+
+    String cliente;
+    int contaCliente;
+    
+    static int depositaDinheiro (int valor, char cliente){
+    
+        return 0;
+    }
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
