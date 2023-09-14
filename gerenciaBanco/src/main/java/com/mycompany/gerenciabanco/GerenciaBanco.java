18e00ea694e09abfb7fc590d59ebc404055a0593
OgliariNatan <natanogliari@gmail.com>
2023-09-14 09:59:59 -0300

    portifolio

--- gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -16,6 +16,45 @@ public class GerenciaBanco {
     
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        
+        Scanner menu = new Scanner (System.in);
+        
+       
+        while (true){
+             System.out.print("##--Teste Estrutura de Menu--##\n\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("| Opção 1 - Novo Cadastro     |\n");
+            System.out.print("| Opção 2 - Clientes          |\n");
+            System.out.print("| Opção 3 - Produtos          |\n");
+            System.out.print("| Opção 4 - Sair              |\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("Digite uma opção: ");
+            int opcao = menu.nextInt();// lê a opção do cliente
+        
+                  if (opcao == 4) {
+                System.out.print("\nAté logo!");
+                menu.close(); 
+            }
+
+            switch (opcao) {
+            case 1:
+                System.out.print("\nOpção Novo Cadastro Selecionado");
+                break;
+
+            case 2:
+                System.out.print("\nOpção Clientes Selecionado\n");
+                break;
+
+            case 3:
+                System.out.print("\nOpção Produtos Selecionado\n");
+                break;
+
+            default:
+                System.out.print("\nOpção Inválida!");
+                break;
+             }
+        
+        }
     }
 }
