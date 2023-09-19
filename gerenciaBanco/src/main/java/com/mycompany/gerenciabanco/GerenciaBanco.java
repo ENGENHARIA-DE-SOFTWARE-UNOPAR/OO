af18aa03df307c78c8467cae2b5eb90b6f06ae9e
OgliariNatan <natanogliari@gmail.com>
2023-09-19 10:41:17 -0300

    gerenciBanco

--- gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -1,6 +1,8 @@
 package com.mycompany.gerenciabanco;
+
 import java.util.*;
 import javax.swing.JOptionPane;
+import java.lang.Integer;
 /**
  *
  * @author Natan Ogliari
@@ -13,9 +15,10 @@ public class GerenciaBanco {
         String nome = menu.next();// lê O nome do cliente
         System.out.println("Informe o seu Sobrenome:\n");
         String sobrenome = menu.next();// lê O sobrenome do cliente
+// lê O sobrenome do cliente
         System.out.println("Informe o seu numero de CPF:\t xxx.xxx.xxx-xx:\n");
-        int cpf = menu.nextIn();// lê numero de CPF    
-        return 0;
+        // lê numero de CPF    
+        
     }
     
     static int depositaDinheiro (int valor, char cliente){
@@ -29,23 +32,38 @@ public class GerenciaBanco {
                 
     }
     
+    public static void realizaSaque(){
+     
+    
+    }
+    
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         
         Scanner menu = new Scanner (System.in);
-        
+        ArrayList<String> clientes = new ArrayList<String>();
        
         while (true){
-            System.out.print("|---------------------------------|\n");
-            System.out.print("| Opção 1 - Consulta saldo        |\n");
-            System.out.print("| Opção 2 - Realizar um deposito  |\n");
-            System.out.print("| Opção 3 - Realizar um saque     |\n");
-            System.out.print("| Opção 4 - Sair                  |\n");
-            System.out.print("|---------------------------------|\n");
-            System.out.print("Digite uma opção: ");
-            int opcao = menu.nextInt();// lê a opção do cliente
-        
-                  if (opcao == 4) {
+            //System.out.print("|---------------------------------|\n");
+           // System.out.print("| Opção 1 - Consulta saldo        |\n");
+           //// System.out.print("| Opção 2 - Realizar um deposito  |\n");
+           // System.out.print("| Opção 3 - Realizar um saque     |\n");
+           // System.out.print("| Opção 4 - Sair                  |\n");
+           // System.out.print("|---------------------------------|\n");
+            //System.out.print("Digite uma opção: ");
+            //int opcao = menu.nextInt();// lê a opção do cliente
+            //Integer.parseInt
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(null,"|------------------------------------|\n | Opção 1 - Consulta saldo         |\n| Opção 2 - Realizar um deposito  |\n| Opção 3 - Realizar um saque     |\n| Opção 4 - Sair                  |\n|---------------------------------|"));
+            
+            System.out.println(opcao);
+            
+            if ("opcao" == null){// caso o usuario cncele a opção
+                System.out.print("\n Você cancelou o comando\n");
+                menu.close();
+                break;            
+            }
+            
+            if (opcao == 4) {
                 System.out.print("\nAté logo!");
                 menu.close(); // fecha o Scanner
                 break;//sai do while
